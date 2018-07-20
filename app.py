fc5ed2f08f115f66a690fcf0f362c0238298dbbe
TEJAS AGARWAL <tejasagarwal9897@gmail.com>
2018-07-20 22:13:49 +0530

    adding changes

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    # configuration property
 app.secret_key =  "tejas"  # The secret key is used to encrypt the JWT. That is how we encrypt the user info into a secured token
 api = Api(app)
 
-@app.before_first_request      # runs the method below it before the first request into this app
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)                         # JWT creates a new endpoint '/auth'
 
 api.add_resource(Store,'/store/<string:name>')
